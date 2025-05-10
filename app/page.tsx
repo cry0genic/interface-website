2e1496888db572b6a30319457e9c7b304a8f024f
dibkb <dibas9110@gmail.com>
2025-05-10 11:29:00 +0530

    grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 /* eslint-disable @next/next/no-img-element */
 import { Darker_Grotesque } from "next/font/google";
-import background from "../public/background.svg";
-import { Invoice, RfqSlip, Grn } from "./_components/svgs";
-import { SapLogo, AmazonLogo, UnfiLogo } from "./_components/amzon-logo";
-import { LinkedinSvg, Xsvg } from "./_components/svg";
-import { Logo } from "./_components/logo";
 
 const darkerGrotesque = Darker_Grotesque({
   variable: "--font-darker-grotesque",
@@ -16,71 +11,13 @@ export default function Home() {
     <div
       className="w-screen h-screen absolute p-4 overflow-hidden"
       style={{
-        backgroundImage: `url(${background.src})`,
-        backgroundSize: "cover",
+        backgroundImage:
+          "radial-gradient(circle, #E6E6E6 1px, transparent 1px)",
+        backgroundSize: "20px 20px",
         backgroundPosition: "center",
       }}
     >
-      <div className="absolute top-6 xl:w-[69%] w-[90%] left-1/2 -translate-x-1/2 rounded-lg flex items-center justify-between bg-black z-50 px-4 py-4">
-        <div className="items-center gap-2 hidden sm:flex">
-          <button
-            onClick={() => {
-              window.open("https://beta.getinterface.tech/login", "_blank");
-            }}
-            className="bg-white cursor-pointer hover:bg-neutral-200 text-black px-4 py-2 rounded-lg font-medium transition-colors duration-300"
-          >
-            Login
-          </button>
-        </div>
-        <Logo />
-        <button
-          className="bg-[#3EC76A] text-black px-4 py-2 rounded-lg font-medium cursor-pointer"
-          onClick={() => {
-            window.open(
-              "https://cal.com/rebhav-bharadwaj/introductory-chat",
-              "_blank"
-            );
-          }}
-        >
-          Get in touch
-        </button>
-      </div>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-1 items-center z-50">
-        <h3
-          className={`${darkerGrotesque.className} md:text-[68px] text-[10vw] font-bold whitespace-nowrap`}
-        >
-          Reimagining Services.
-        </h3>
-      </div>
-      <RfqSlip />
-      <Invoice />
-      <Grn />
-
-      <AmazonLogo />
-      <SapLogo />
-      <UnfiLogo />
-      <div className="absolute bottom-4 w-full text-center hidden sm:block">
-        <a
-          href="/privacy-policy.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-neutral-400 font-semibold hover:text-[#51eb82] text-sm transition-colors duration-300"
-        >
-          Privacy Policy
-        </a>
-      </div>
-
-      <div className="absolute md:bottom-4 sm:right-16 bottom-4 right-[50%] translate-x-1/2 sm:translate-x-0 flex gap-2 items-center">
-        <a href="https://x.com/getinterface" target="_blank">
-          <Xsvg />
-        </a>
-        <a
-          href="https://www.linkedin.com/company/interface-ai-labs/"
-          target="_blank"
-        >
-          <LinkedinSvg />
-        </a>
-      </div>
+      fsdf
     </div>
   );
 }
